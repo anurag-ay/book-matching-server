b34cae619413f0d592c9a2329af9c5057fb84b3e
anurag-ay <ay1909069@gmail.com>
2023-12-10 00:34:23 +0530

    Fix bugs

--- routes/recommend.js
+++ routes/recommend.js
@@ -5,26 +5,30 @@ const Student = require("../models/Student");
 const mostSimilarVector = require("../utils/recommendationAlgo");
 
 router.post("/", async (req, res) => {
-  const { personalityVector, userName } = req.body;
+  try {
+    const { personalityVector, userName } = req.body;
 
-  const books = await Book.find();
-  const score = mostSimilarVector(personalityVector, books);
-  const scoreBook = [];
+    const books = await Book.find();
+    const score = mostSimilarVector(personalityVector, books);
+    const scoreBook = [];
 
-  for (let book of score) {
-    scoreBook.push(book._id);
+    for (let book of score) {
+      scoreBook.push(book._id);
+    }
+    const genres = score[0].genre.split(",").map((genre) => genre.trim());
+    const updateData = {
+      recommendedGenres: genres,
+      personalityVector: personalityVector,
+      readBooks: scoreBook,
+    };
+    await Student.findOneAndUpdate({ userName: userName }, updateData, {
+      new: true,
+    });
+
+    res.status(200).send(score);
+  } catch (err) {
+    res.status(500).send(err);
   }
-  const genres = score[0].genre.split(",").map((genre) => genre.trim());
-  const updateData = {
-    recommendedGenres: genres,
-    personalityVector: personalityVector,
-    readBooks: scoreBook,
-  };
-  await Student.findOneAndUpdate({ userName: userName }, updateData, {
-    new: true,
-  });
-
-  res.status(200).send(score);
 });
 
 module.exports = router;
